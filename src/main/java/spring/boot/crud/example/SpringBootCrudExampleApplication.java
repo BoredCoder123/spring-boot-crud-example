1279fc6ee046b665bda1d1e7a6d330daf7498819
Ankit Kathait <ankitkathait28@gmail.com>
2021-05-24 02:31:27 +0530

    Revert from test

--- src/main/java/spring/boot/crud/example/SpringBootCrudExampleApplication.java
+++ src/main/java/spring/boot/crud/example/SpringBootCrudExampleApplication.java
@@ -19,8 +19,6 @@ public class SpringBootCrudExampleApplication {
 
 		ConsoleHandler handler = new ConsoleHandler();
 
-
-
 		LogFormatter formatter = new LogFormatter();
 		handler.setFormatter(formatter);
 
